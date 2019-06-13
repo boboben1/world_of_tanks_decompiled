ccd94826696aa4756546af11a52967559ac67c23
boboben1 <boboben5@gmail.com>
2019-06-13 13:13:02 -0400

    Added 1.5.1.1428 and fixed bugs

--- decompile.py
+++ decompile.py
@@ -4,9 +4,11 @@ import shutil
 import errno
 import uncompyle6
 import logging
+import sys
+import threading
 
 logging.basicConfig(filename='decompile.log', filemode='w',
-                    format='%(name)s - %(levelname)s - %(message)s')
+                    format='%(name)s - %(levelname)s - %(message)s', level="INFO")
 
 
 def copyFile(src, dest):
@@ -35,33 +37,58 @@ def find_files(directory, pattern):
                 yield filename
 
 
-logging.info("Decompiling files")
+def main():
 
-errors = 0
+    logging.info("Decompiling files")
 
-for filename in (filename for filename in find_files('scripts', "*") if filename.endswith(".pyc")):
-    basename = os.path.splitext(os.path.basename(filename))[0]
-    dirname = os.path.dirname(filename)
-    with openAndMakeDirs("decompiled\\%s\\%s.py" % (dirname, basename), "w") as fileobj:
-        try:
-            uncompyle6.uncompyle_file(filename, fileobj)
-        except:
-            logging.error("Error occurred decompiling file: %s" % filename)
-            errors += 1
+    errors = 0
 
-if errors > 0:
-    logging.warning(
-        "Decompiling some files failed! Check decompile.log for details")
-else:
-    logging.info("All files decompiled successfully!")
+    PYC = []
 
-logging.info("Moving other files")
+    for filename in (filename for filename in find_files('scripts', "*") if filename.endswith(".pyc")):
+        PYC.append(filename)
 
-List = []
+    with open("allpyc.txt", "w") as f:
+        for item in PYC:
+            f.write("%s\n" % item)
+    try:
+        for filename in PYC:
+            basename = os.path.splitext(os.path.basename(filename))[0]
+            dirname = os.path.dirname(filename)
+            try:
+                fileobj = openAndMakeDirs("decompiled\\%s\\%s.py" %
+                                          (dirname, basename), "w")
+                uncompyle6.uncompyle_file(filename, fileobj)
+                #print("Decompiled file %s" % (filename))
+                fileobj.close()
+            except (uncompyle6.semantics.pysource.SourceWalkerError, uncompyle6.parser.ParserError):
+                logging.error("Error occurred decompiling file: %s" % filename)
+                errors += 1
+    except Exception as e:
+        print(e.__class__.__name__)
+        logging.error(str(e))
 
-for filename in (filename for filename in find_files('scripts', "*") if not filename.endswith(".pyc")):
-    List.append(filename)
+    if errors > 0:
+        logging.warning(
+            "Decompiling some files failed! Check decompile.log for details")
+    else:
+        logging.info("All files decompiled successfully!")
 
-for item in List:
-    copyFile(item, "decompiled\\%s\\%s" %
-             (os.path.dirname(item), os.path.basename(item)))
+    logging.info("Moving other files")
+
+    List = []
+
+    for filename in (filename for filename in find_files('scripts', "*") if not filename.endswith(".pyc")):
+        List.append(filename)
+
+    for item in List:
+        copyFile(item, "decompiled\\%s\\%s" %
+                 (os.path.dirname(item), os.path.basename(item)))
+
+
+# Took forever to figure this out
+if __name__ == '__main__':
+    sys.setrecursionlimit(100000)
+    threading.stack_size(200000000)
+    thread = threading.Thread(target=main)
+    thread.start()
